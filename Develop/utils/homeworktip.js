a2948e9dfc639315fc8594c4d9e25f4b37bc2598
Eric Cole <ecole052785@gmail.com>
2021-04-15 18:47:29 -0400

    moved files from repo to locally created repo

---
+++ Develop/utils/homeworktip.js
@@ -0,0 +1,27 @@
+// homework
+
+// const inquirer = require('inquirer');
+
+// inquirer 
+//     .prompt ([
+//         {
+//             type: 'input', 
+//             message: 'what is your username?',
+//             name: 'username',
+//         },
+//         {
+//             type: 'password', 
+//             message: 'what is your password?',
+//             name: 'password',
+//         },
+//         {
+//             type: 'password', 
+//             message: 'what is your password?',
+//             name: 'confirm',
+//         },
+//     ])
+//     .then((response) => 
+//     response.confirm === response.password 
+//     ? console.log('success')
+//     : console.log('You forgot your password already.')
+//     );
