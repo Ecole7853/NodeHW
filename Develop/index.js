ff667530aa9516b43c1150e0668978030d037726
Eric Cole <ecole052785@gmail.com>
2021-04-15 18:55:38 -0400

    Question framework completed

--- Develop/index.js
+++ Develop/index.js
@@ -1,14 +1,29 @@
 // TODO: Include packages needed for this application
 const fs = require('fs'); 
-
+const inquirer = require('inquirer');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    What is your GitHub username?
+    What is your Email address?
+    What is your project name?
+    Please write a short description of your project.
+    What kind of license should your project have?
+    What command should be run to install dependencies?
+    What command should be run to run tests?
+    What does the user need to know about using the repo?
+    What does the user need to know about contributing to the repo?
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+
+    inquirer.prompt(questions).then(data){
+
+    }
+}
 
 // Function call to initialize app
 init();
