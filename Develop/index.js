abc5c667c94a91882ced5bbbc83c8877318eb4d6
Eric Cole <ecole052785@gmail.com>
2021-04-15 19:08:16 -0400

    questions added

--- Develop/index.js
+++ Develop/index.js
@@ -3,15 +3,51 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 // TODO: Create an array of questions for user input
 const questions = [
-    What is your GitHub username?
-    What is your Email address?
-    What is your project name?
-    Please write a short description of your project.
-    What kind of license should your project have?
-    What command should be run to install dependencies?
-    What command should be run to run tests?
-    What does the user need to know about using the repo?
-    What does the user need to know about contributing to the repo?
+    {
+        type: 'input',
+        message: 'What is your GitHub username?',
+        name: 'Github',
+      },
+      {
+        type: 'input',
+        message: 'What is you What is your Email address?',
+        name: 'Email',
+      },
+      {
+        type: 'input',
+        message: 'What is your project name?',
+        name: 'Project',
+      },
+      {
+        type: 'input',
+        message: 'Please write a short description of your project.',
+        name: 'description',
+      },
+      {
+        type: 'input',
+        message: ' What kind of license should your project have?',
+        name: 'license',
+      },
+      {
+        type: 'input',
+        message: 'What command should be run to install dependencies?',
+        name: 'dependencies',
+      },
+      {
+        type: 'input',
+        message: 'What command should be run to run tests?',
+        name: 'tests',
+      },
+      {
+        type: 'input',
+        message: 'What does the user need to know about using the repo?',
+        name: 'usingrepo',
+      },
+      {
+        type: 'input',
+        message: 'What does the user need to know about contributing to the repo?',
+        name: 'contributingrepo',
+      }, 
 ];
 
 // TODO: Create a function to write README file
@@ -20,7 +56,8 @@ function writeToFile(fileName, data) {}
 // TODO: Create a function to initialize app
 function init() {
 
-    inquirer.prompt(questions).then(data){
+    inquirer.prompt(questions).then(data);
+    {
 
     }
 }
